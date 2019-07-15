91a72113c8085de4704d90a46d71a9e64363c27a
homirun <hatunemiku.moco@gmail.com>
2019-07-16 02:29:42 +0900

    READMEの追加, main.pyに最低限の機能を実装

---
+++ README.md
@@ -0,0 +1,13 @@
+# bus_api_for_zappa_practice
+## Overview
+This Project is zappa practice.
+TUT's school bus timetable API.
+## Description
+- Python 3.7.1
+- zappa 0.48.2
+- Flask 1.1.1
+- beautifulsoup4 4.7.1
+- lxml 4.3.4
+
+## Usage 
+ coming soon...
--- src/main.py
+++ src/main.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
+# TODO: 手元の環境ではCDが../srcのため from src import bus_scrにしないと動かない
+from . import bus_scr
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>hello.</h1>'
+    return '<h1>This page is unofficial TUT school bus timetable api.</h1>'
+
+
+@app.route('/timetable/hm/')
+def hm_timetable():
+    scr = bus_scr.get_timetable("hm")
+    return jsonify(scr)
+
+
+@app.route('/timetable/h/')
+def h_timetable():
+    scr = bus_scr.get_timetable("h")
+    return jsonify(scr)
+
+
+@app.route('/timetable/gk/')
+def gs_timetable():
+    scr = bus_scr.get_timetable("gk")
+    return jsonify(scr)
 
 
 if __name__ == '__main__':
+    import sys
+    import pprint
+
+    pprint.pprint(sys.path)
+
     app.run(debug=False, host='0.0.0.0', port=80)
